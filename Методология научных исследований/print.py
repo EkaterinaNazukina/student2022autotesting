b8e0ff3f04bc7e7e0dc696aed92e4b89b5f1923c
EkaterinaNazukina <116084429+EkaterinaNazukina@users.noreply.github.com>
2022-10-20 18:58:14 +0300

    План магистерской работы

---
+++ Методология научных исследований/print.py
@@ -0,0 +1,29 @@
+#print ("Екатерина")
+#from os import lstat
+
+
+#def change_one_smb(line, smb, pos):
+#lstat[0] = smb
+#lst[pos] = smb
+#return ''.join
+
+#print(lst)
+#line = ''.join(line)
+#print(line)
+
+
+#line = "qwerty"
+#smb = '#'
+#pos = 0
+#change_one_smb(line, smb, pos)
+#print(line)
+
+#for i in range(len(line))
+#print(line[2:4])
+#print(line[2:-1])
+#int()
+#float()
+#list()
+#tuple()
+#dict()
+#bool()
